0512f56ec6283727ebec5e16156429cb2c8d120f
Walid Omar <walidomar291@gmail.com>
2024-12-18 07:03:12 +0200

    navbar component and category list component angular

--- UI/CodePulse/src/app/app.routes.ts
+++ UI/CodePulse/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './features/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainPageComponent
+    },
+    {
+        path: 'admin/categories',
+        component: CategoryListComponent
+    }
+];
