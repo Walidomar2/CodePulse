43909faf6d60ac5b3b00ed17454511ef4df81f2d
Walid Omar <walidomar291@gmail.com>
2025-01-11 03:44:13 +0200

    BE - login action method and token generating

--- APIs/CodePulse.API/CodePulse.API/Controllers/AuthController.cs
+++ APIs/CodePulse.API/CodePulse.API/Controllers/AuthController.cs
@@ -10,12 +10,12 @@ namespace CodePulse.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
-        public AuthController(UserManager<IdentityUser> userManager)
+        private readonly ITokenRepository _tokenRepository;
+        public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
         {
             _userManager = userManager;
+            _tokenRepository = tokenRepository;
         }
-
-        
 
         [HttpPost]
         [Route("register")]
@@ -62,6 +62,36 @@ namespace CodePulse.API.Controllers
             return ValidationProblem(ModelState);
         }
 
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
+        {
+            var identityUser = await _userManager.FindByEmailAsync(loginRequest.Email);
+
+            if(identityUser is not null)
+            {
+                var checkPasswordResult = await _userManager.CheckPasswordAsync(identityUser, loginRequest.Password);
+
+                if(checkPasswordResult)
+                {
+                    var roles = await _userManager.GetRolesAsync(identityUser);
+                    var token = _tokenRepository.CreateJwtToken(identityUser, roles.ToList());
+
+                    var response = new LoginResponseDto()
+                    {
+                        Email = loginRequest.Email,
+                        Roles = roles.ToList(),
+                        Token = token
+
+                    };
+
+                    return Ok(response);
+                }
+            }
+            ModelState.AddModelError("", "Email or Password Incorrect");
+
+            return ValidationProblem(ModelState);
+        }
 
     }
 }
---
+++ APIs/CodePulse.API/CodePulse.API/Repositories/TokenRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CodePulse.API.Repositories
+{
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly IConfiguration _configuration;
+        public TokenRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public string CreateJwtToken(IdentityUser user, List<string> roles)
+        {
+            // 1 - Creating Claims 
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            // 2 - Creating a Symmetric security key 
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            // 3 - Creating the credentials using the generated key and encrypt it 
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // 4 - Generating the Token
+            var token = new JwtSecurityToken(
+               issuer: _configuration["Jwt:Issuer"],
+               audience: _configuration["Jwt:Audience"],
+               claims: claims,
+               expires: DateTime.Now.AddMinutes(15),
+               signingCredentials: credentials
+               );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
